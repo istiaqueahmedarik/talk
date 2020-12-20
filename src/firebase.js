b89abe3fa2be2122ed8b4a37c318cdbc98e0d5a7
istiaqueahmedarik <69030256+istiaqueahmedarik@users.noreply.github.com>
2020-12-20 15:00:04 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,7 @@ import firebase from "firebase";
 const firebaseApp = firebase.initializeApp({
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-    apiKey: "AIzaSyDYTGQdZsgLfZ6LnkisH-WgOeozBCSwupc",
-  authDomain: "messenger-d3559.firebaseapp.com",
-  databaseURL: "https://messenger-d3559.firebaseio.com",
-  projectId: "messenger-d3559",
-  storageBucket: "messenger-d3559.appspot.com",
-  messagingSenderId: "464109178666",
-  appId: "1:464109178666:web:a5d635a3b07d029e515853",
-  measurementId: "G-R9BWPDQXLE"
+    // your config
   
 });
 const db = firebaseApp.firestore();
